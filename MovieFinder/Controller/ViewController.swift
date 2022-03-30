58455581370c52a407c8212161bce6d94e239670
Lina <lina@Luchos-MacBook-3.local>
2022-03-30 17:40:52 -0500

    Fisrt working version

---
+++ MovieFinder/Controller/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  MovieFinder
+//
+//  Created by Lina on 28/03/22.
+//
+
+import UIKit
+import Kingfisher
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var imageTest: UIImageView!
+    
+    private var movieManager = MovieManager()
+    private var posterManager = PosterManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        movieManager.delegate = self
+        searchTextField.delegate = self
+    }
+}
+
+//MARK: - UITextFieldDelegate
+
+extension ViewController: UITextFieldDelegate{
+    
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != ""{
+            return true
+        } else {
+            textField.placeholder = "Enter movie title to search"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let movie = searchTextField.text {
+            movieManager.fetchMovie(movieTitle: movie)
+        }
+        searchTextField.text = ""
+    }
+    
+}
+
+//MARK: - MovieManagerDelegate
+
+extension ViewController: MovieManagerDelegate{
+    func didUpdateMovie(_ movieManager: MovieManager, movie: MovieModel) {
+        print(movie.overview)
+        
+        DispatchQueue.main.async {
+            if let poster = movie.posterPath {
+                let posterURL = URL(string: self.posterManager.fetchPosterURL(posterPath: poster))
+                self.imageTest.kf.setImage(with: posterURL)
+            }
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
